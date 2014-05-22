e81fa3d5c60a96abfff3c220836021a461736ae6
Praseetha-KR <voidimagineer@yahoo.com>
2014-05-22 18:47:03 +0530

    Form to enter choice, view for vote, code optimization with classes

--- quipper/views.py
+++ quipper/views.py
@@ -1,19 +1,42 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.template import RequestContext, loader
-from quipper.models import Poll
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
 
-def index(request):
-	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-	context = {'latest_poll_list': latest_poll_list}
-	return render(request, 'quipper/index.html', context)
+from quipper.models import Choice, Poll
 
-def detail(request, poll_id):
-	poll = get_object_or_404(Poll, pk=poll_id)
-	return render(request, 'quipper/detail.html', {'poll': poll})
+class IndexView(generic.ListView):
+	template_name = 'quipper/index.html'
+	context_object_name = 'latest_poll_list'
 
-def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+	def get_queryset(self):
+		return Poll.objects.order_by('-pub_date')[:5]		
+	# latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+	# context = {'latest_poll_list': latest_poll_list}
+	# return render(request, 'quipper/index.html', context)
+
+class DetailView(generic.DetailView):
+	model = Poll
+	template_name = 'quipper/detail.html'
+	# poll = get_object_or_404(Poll, pk=poll_id)
+	# return render(request, 'quipper/detail.html', {'poll': poll})
+
+class ResultsView(generic.DetailView):
+	model = Poll
+	template_name = 'quipper/results.html'
+    # poll = get_object_or_404(Poll, pk=poll_id)
+    # return render(request, 'quipper/results.html', {'poll': poll})
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+	p = get_object_or_404(Poll, pk=poll_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'quipper/detail.html', {
+			'poll': p,
+			'error_message': "You didn't select a choice."
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+    	return HttpResponseRedirect(reverse('quipper:results', args=(p.id,)))
